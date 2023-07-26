2fce8efecfeb2555e344201f24eff8d053b82cc7
Alex MacArthur <alex.macarthur@ramseysolutions.com>
2023-07-26 12:08:03 -0500

    Update Steppp in React package.

--- packages/steppp-react/example/app.tsx
+++ packages/steppp-react/example/app.tsx
@@ -7,6 +7,8 @@ const options = {
 }
 
 function App() {
+  const [showExtraStep, setShowExtraStep] = React.useState(false);
+
   return (
     <Provider options={options}>
       {({ moveTo, forward, backward }) => (
@@ -19,10 +21,15 @@ function App() {
             <SubStep>two</SubStep>
             <Step name="third">three</Step>
             <Step name="fourth">fourth</Step>
+
+            { showExtraStep && <Step name="dynamic_step">DYNAMIC</Step>}
           </Wrapper>
 
           <button onClick={forward}>Forward</button>
           <button onClick={backward}>Backward</button>
+
+          <button onClick={() => setShowExtraStep(true)}>Show Extra Step</button>
+          <button onClick={() => moveTo('dynamic_step')}>Go to Dynamic Step</button>
         </>
       )}
     </Provider>
