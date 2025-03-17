6c8f8517fd5eae9037d4a56f8cd76ff2a293743c
Alex MacArthur <alex@macarthur.me>
2025-03-17 15:56:37 -0500

    Use peer dependencies.

--- packages/steppp-react/vite.config.js
+++ packages/steppp-react/vite.config.js
@@ -1,6 +1,8 @@
 import { defineConfig } from "vite";
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
+  plugins: [react()],
   test: {
     environment: "jsdom",
     setupFiles: "../../setupTests.js",
